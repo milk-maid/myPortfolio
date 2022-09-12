1af120f8f07ba1e05159ba1439822ede0b8e3fbc
Bello Roqeeb <roqbell@gmail.com>
2022-09-12 18:24:36 +0100

    serious

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-pay comjhjls
+var card = document.querySelector("#card");
+
+function openRegister() {
+    card.style.transform = "rotateY(-180deg)";
+}
+
+function openLogin() {
+    card.style.transform = "rotateY(0deg)";
+}
